4a0740cd3def2cdf7063b89179a290ca7182dc5d
promoteindia <99000790+promoteindia@users.noreply.github.com>
2022-02-15 19:55:59 +0530

    fourthchanges

--- sample_code.sh
+++ sample_code.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-
+https://github.com/promoteindia/devops-helloworld/blob/master/sample_code.sh
 echo "This is an Samples Script from GitHub"
-echo "This is test from Kanaks-promotions Server"
-echo "This is an example promoteindia-kanaks script"
+echo "This is test from Kanaksindpromotions Server"
+echo "This is an example promoteind-kanaks script"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
