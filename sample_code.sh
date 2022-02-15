be8259830a5d0a2ef536893be436fab002943b3e
promoteindia <99000790+promoteindia@users.noreply.github.com>
2022-02-15 19:29:48 +0530

    This is second commit

--- sample_code.sh
+++ sample_code.sh
@@ -2,8 +2,8 @@
 #
 
 echo "This is an Samples Script from GitHub"
-echo "This is test from Kanaks Server"
-echo "This is an example promoteindia script"
+echo "This is test from Kanaks-promotions Server"
+echo "This is an example promoteindia-kanaks script"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
